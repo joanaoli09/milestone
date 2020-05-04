4e44b511008f31395e88bb63dcbd4e2077d40d8c
Joana Leitão Oliveira <joanaleitaooliveira@Air-de-Joana.lan>
2020-05-04 14:14:41 +0100

    Updates

---
+++ precache-manifest.cf7561fdf2534d89b4e6cee7ac8b6633.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79fb7fa2de431822e7c7f9d357a18534",
+    "url": "/milestone/index.html"
+  },
+  {
+    "revision": "d3cd9a7d48084d69ab9d",
+    "url": "/milestone/static/css/main.e08544b3.chunk.css"
+  },
+  {
+    "revision": "b2957d636d0fb2dfc04c",
+    "url": "/milestone/static/js/2.e39b19ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/milestone/static/js/2.e39b19ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3cd9a7d48084d69ab9d",
+    "url": "/milestone/static/js/main.7d82d769.chunk.js"
+  },
+  {
+    "revision": "76f0a5b03a5061c4b130",
+    "url": "/milestone/static/js/runtime-main.14e83c28.js"
+  }
+]);
